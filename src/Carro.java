f35d34a405aed35bdaf4b27e54aefe0d727d8ebe
Robson Martins <robsonsmartins@gmail.com>
2022-10-26 10:59:39 -0300

    Criando Projeto Inicial

---
+++ src/Carro.java
@@ -0,0 +1,53 @@
+public class Carro {
+    
+    private int id;
+    private String nome;
+    private String marca;
+    private String cor;
+    private float preco;
+    
+    public Carro() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    
+    
+    
+}
